4b7cf6c95e087ca155fd8feaaaab6049dd633410
Allan <allanle@shaw.ca>
2021-01-12 16:16:27 -0800

    initial commit

---
+++ src/main/java/com/parabank/app/App.java
@@ -0,0 +1,13 @@
+package com.parabank.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
